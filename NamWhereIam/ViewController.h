bed823f3c77049909663e10d25d8470c05416059
Namrata Mahajan <nammahajan12@gmail.com>
2016-10-12 15:38:04 +0530

    Initial Commit

---
+++ NamWhereIam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NamWhereIam
+//
+//  Created by Namrata on 12/10/16.
+//  Copyright © 2016 Namrata Mahajan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ NamWhereIam/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NamWhereIam
+//
+//  Created by Namrata on 12/10/16.
+//  Copyright © 2016 Namrata Mahajan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
